7450cb99b06df1413a498ac38a2589a7030f2abc
Ned Lowe <ned@amaas.com>
2017-09-10 13:05:36 +0800

    AMAAS-1089 - Support holiday overrides from a file in the repo for when workalendar can't update fast enough.  It is possible that this won't be fast enough either - in which case we could store the overrides on DynamoDB or something like that.

--- fincalendar/holidays.py
+++ fincalendar/holidays.py
@@ -7,26 +7,13 @@ from functools import lru_cache
 from dateutil.parser import parse
 
 from fincalendar.holiday_mapping import get_calendar
-
-@lru_cache(maxsize=128)
-def get_overrides():
-    # READ FILE
-    results = {"SGP": { 
-               "add": {parse("2015-5-27").date(), parse("2016-5-27").date()},
-               "subtract": {parse("2015-12-25").date()}
-               },
-               "HKG": { 
-               "add": {parse("2015-5-27").date(), parse("2016-5-27").date()}
-               },
-               "GBR": { 
-               "subtract": {parse("2015-12-25").date()}
-               }}
-    return results
+from fincalendar.holiday_overrides import OVERRIDES
 
 def holidays_set(alpha_3_country_code, year, state_code=None):
     """ TODO - Add state_code handling """
-    overrides = get_overrides().get(alpha_3_country_code) or {}
+    overrides = OVERRIDES.get(alpha_3_country_code) or {}
     calendar = get_calendar(alpha_3_country_code=alpha_3_country_code)
     holidays = calendar.holidays_set(year=year)
-    holidays = holidays | overrides.get("add", set()) - overrides.get("subtract", set())
+    holidays |= overrides.get("add", set())
+    holidays -= overrides.get("subtract", set())
     return holidays
--- tests/holidays.py
+++ tests/holidays.py
@@ -1,31 +1,50 @@
-import unittest
 from datetime import date
+from mock import patch
+import unittest
 
+import fincalendar.holidays
 from fincalendar.holidays import holidays_set
 
+
+DUMMY_OVERRIDES = {"SGP": {"add": {date(2015, 5, 27), date(2016, 5, 27)},
+                           "subtract": {date(2015, 12, 25)}
+                          },
+                   "HKG": {"add": {date(2015, 5, 27), date(2016, 5, 27)}
+                          },
+                   "GBR": {"subtract": {date(2015, 12, 25)}
+                          }
+                  }
 
 class HolidaysTest(unittest.TestCase):
 
     def setUp(self):
         pass
-    
-    def tearDown(self):
-        pass
 
+    @patch("fincalendar.holidays.OVERRIDES", DUMMY_OVERRIDES)
     def test_Overrides(self):
-        print(holidays_set(alpha_3_country_code='SGP', year=2015))
+        holidays = holidays_set(alpha_3_country_code='SGP', year=2015)
+        self.assertIn(date(2015, 5, 27), holidays)
 
+    @patch("fincalendar.holidays.OVERRIDES", DUMMY_OVERRIDES)
     def test_NoOverrides(self):
-        holidays_set(alpha_3_country_code='MYS', year=2015)
+        holidays = holidays_set(alpha_3_country_code='MYS', year=2015)
+        self.assertIn(date(2015, 12, 25), holidays)
 
+    @patch("fincalendar.holidays.OVERRIDES", DUMMY_OVERRIDES)
     def test_MissingCountryCode(self):
-        holidays_set(alpha_3_country_code='XXX', year=2015)
+        # The default calendar in workalendar has the 1st Jan as a holiday
+        holidays = holidays_set(alpha_3_country_code='XXX', year=2015)
+        self.assertEqual(holidays, {date(2015, 1, 1)})
 
+    @patch("fincalendar.holidays.OVERRIDES", DUMMY_OVERRIDES)
     def test_OverridesOnlyAdd(self):
-        print(holidays_set(alpha_3_country_code='HKG', year=2015))
+        holidays = holidays_set(alpha_3_country_code='HKG', year=2015)
+        self.assertIn(date(2015, 5, 27), holidays)
 
+    @patch("fincalendar.holidays.OVERRIDES", DUMMY_OVERRIDES)
     def test_OverridesOnlySubtract(self):
-        holidays_set(alpha_3_country_code='GBR', year=2015)
+        holidays = holidays_set(alpha_3_country_code='GBR', year=2015)
+        self.assertNotIn(date(2015, 12, 25), holidays)
 
 if __name__ == '__main__':
     unittest.main()
